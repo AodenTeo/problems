ebf86ffb226c63c52eab9a5ea862ddeef3b91276
JOANNE <user@Users-Mac.local>
2019-02-13 17:45:23 +0800

    Added bell() to arithmetic.js

--- problems-backend/arithmetic.js
+++ problems-backend/arithmetic.js
@@ -1,2 +1,26 @@
 const form = require('./generalFunctions');
 
+function bell() {
+    let bellTime1;
+    let bellTime2;
+    let  bellTime3;
+    let commonality = form.rand(5);
+    do {
+        bellTime1 = commonality*(form.rand(20) + 3);
+        bellTime2 = commonality*(form.rand(20) + 3);
+        bellTime3 = form.rand(60) + 10;
+    } while (bellTime1 === bellTime2 || bellTime1 == bellTime3 || bellTime2 === bellTime3);
+    const time = form.rand(10);
+    const day = form.rand(20);
+    const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August','September','October', 'November', 'December'][Math.floor(Math.random()*11 + 1)];
+    let string = `Three wolves howl at regular intervals of ${bellTime1} minuites, ${bellTime2} minuites, and ${bellTime3} minuites respectively. Given that they howl together at ${time} on the ${day}th of ${month}, how long in minuites will it be before they howl together again.`;
+    let answerString = `Taking the lowest commen multiple of ${bellTime1}, ${bellTime2}, and ${bellTime3} we get ${form.lcm([bellTime1, bellTime2, bellTime3])}. Thus, the next time that they howl together wil be in ${form.lcm([bellTime1, bellTime2, bellTime3])} minuites`;
+    console.log(string);
+    console.log(answerString);
+    return [string, answerString];
+}
+
+bell();
+
+
+
--- problems-backend/generalFunctions.js
+++ problems-backend/generalFunctions.js
@@ -1,7 +1,7 @@
 const names = ['Tom', 'Peter', 'Sue', 'Denise', 'Rodger', 'Anne', 'Lucy', 'Lily', 'David', 'Joanne', 'Elizabeth', 'James', 'Bob', 'Michael', 'Albert', 'Stephen', 'Steve', 'Bill', 'Catherine', 'Emma', 'Emily', 'Elaine', 'Bianka', 'Ethan', 'Eliot', 'Lauren', 'Sam', 'Leonard', 'Nathan', 'Joy', 'Gretchen', 'Lousia', 'Zoey', 'Noah', 'Oliver', 'Benjamin'];
-console.log(names.length);
+
 const swimmingCreature = ['sting ray', 'shark', 'swordfish', 'dolphin', 'megladon', 'mermaid', 'jellyfish', 'submarine', 'spider crab', 'sunfish', 'sea star', 'narwhal', 'fangtooth'];
-console.log(swimmingCreature.length);
+
 
 const form = {
     rand(number) {
@@ -33,7 +33,57 @@ const form = {
           let x = -b/(2*a);
           let y = a*x*x + b*x + c;
           return [x,y];
+      },
+      lcm(input_array) {
+        if (toString.call(input_array) !== "[object Array]")  
+            return  false;  
+     var r1 = 0, r2 = 0;
+        var l = input_array.length;
+        for(i=0;i<l;i++) {
+            r1 = input_array[i] % input_array[i + 1];
+            if(r1 === 0) {
+                input_array[i + 1] = (input_array[i] * input_array[i+1]) / input_array[i + 1];
+            }
+            else {
+                r2 = input_array[i + 1] % r1;
+                if(r2 === 0) {
+                    input_array[i + 1] = (input_array[i] * input_array[i + 1]) / r1;
+                }
+                else {
+                    input_array[i+1] = (input_array[i] * input_array[i + 1]) / r2;
+                }
+            }
+        }
+        return input_array[l - 1];
+    },
+    gcd_two_numbers(x, y) {
+      if ((typeof x !== 'number') || (typeof y !== 'number')) 
+        return false;
+      x = Math.abs(x);
+      y = Math.abs(y);
+      while(y) {
+        var t = y;
+        y = x % y;
+        x = t;
       }
+      return x;
+    },
+    gcd(input) {
+      if (toString.call(input) !== "[object Array]")  
+            return  false;  
+      var len, a, b;
+      len = input.length;
+      if ( !len ) {
+        return null;
+      }
+      a = input[ 0 ];
+      for ( var i = 1; i < len; i++ ) {
+        b = input[ i ];
+        a = this.gcd_two_numbers( a, b );
+      }
+      return a;
+    }
 }
+
 module.exports = form;
 
